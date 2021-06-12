e50a0492a594213c356c292b132153a62a15165c
silvercondor <biocrypto@protonmail.com>
2021-06-13 02:09:58 +0800

    Intiial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Splash from './components/Splash';
+
+function App() {
+
+  return (
+    <div className="App" style={AppStyle}>
+      <Splash/>
+    </div>
+  );
+}
+
+export default App;
+let AppStyle={
+  fontFamily:'Bai Jamjuree, sans-serif',
+}
---
+++ src/components/Splash.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { renderToStaticMarkup } from "react-dom/server";
+import {ReactComponent as BackgroundSVG} from '../assets/Splash/background.svg'
+import LogoSvg from '../assets/Splash/logo.svg'
+
+function Splash() {
+    const svgString = encodeURIComponent(renderToStaticMarkup(<BackgroundSVG />));
+    return (        
+ 
+
+            <div
+            style={{
+                backgroundImage: `url("data:image/svg+xml,${svgString}")`, backgroundSize: "cover", minHeight:'100%', paddingBottom:'200px'
+            }}
+            > 
+            <div className='content-container' style={{marginLeft:'5%', marginRight:'5%', display:'flex', flexDirection:'column'}}>
+            <a href='/' rel='noopener'>
+            <div className='logo' style={{display:'flex', justifyContent:'flex-start', marginTop:'5%'}}>
+                <img src={LogoSvg} style={{maxHeight:"83.22px", justifyContent:"flex-start"}} alt=''/>                
+            </div>
+                </a>
+            <div className='text-container' style={{display:'flex', flexDirection:'column', justifyContent:'flex-start', marginTop:'120px'}}>
+                <div className='splash-header' style={{...titleTextstyle, maxWidth:"50%"}}>
+                    Tokenized real-world ecological assets
+                </div>
+                <div className='splash-content' style={{...contentTextStyle, maxWidth:"50%", marginTop:"100px"}}>
+                The platform to incentivise the collection/uploading of biodiversity genomic data, and labelling/annotation of it which will allow productive processing of data to be done. The platform aims to incentivise the conservation of biodiversity with the creation of revenue streams from the monetisation of this biogenome IP and value.
+                </div>
+                </div>
+            </div>
+            </div>
+
+            
+            
+        
+    )
+}
+export default Splash;
+let titleTextstyle={
+    fontFamily:"Bai Jamjuree, sans serif",
+    fontStyle: "normal",
+    fontWeight: "bold", 
+    fontSize:"72px",
+    color:"#91D873"
+}
+let contentTextStyle={
+    ...titleTextstyle,
+    fontWeight:"500",
+    color:"white",
+    fontSize:"24px"
+}
