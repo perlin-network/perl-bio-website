890115972aea74da5eac772c256144af15aeb1ef
silvercondor <biocrypto@protonmail.com>
2021-06-14 19:05:52 +0800

    Minor changes - Mobile compatability - Switch favicon to svg

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -3,31 +3,37 @@ import React from 'react'
 import { renderToStaticMarkup } from "react-dom/server";
 import {ReactComponent as BackgroundSVG} from '../assets/Splash/background.svg'
 import LogoSvg from '../assets/Splash/logo.svg'
+import TreeSvg from '../assets/Splash/tree.svg'
 
 function Splash() {
     const svgString = encodeURIComponent(renderToStaticMarkup(<BackgroundSVG />));
+    //backgroundImage: `url("data:image/svg+xml,${svgString}")`
     return (        
  
             
             <div
             style={{
-                backgroundColor:'#333336',mixBlendMode:'pass-through',  backgroundImage: `url("data:image/svg+xml,${svgString}")`, backgroundSize: "cover", minHeight:'100vh'
+                backgroundColor:'#333336',mixBlendMode:'pass-through',  backgroundSize: "cover", minHeight:'100vh' 
             }}
             > 
-            <div className='content-container' style={{marginLeft:'5%', marginRight:'5%', display:'flex', flexDirection:'column'}}>
-            <a href='/' rel='noopener'>
-                <Button style={{ width:"100%",justifyContent:"flex-start", borderRadius:"16px",  maxWidth:"376px",marginTop:'5%', maxHeight:"83.22px",}}>
-                    <img src={LogoSvg} style={{minHeight:"83.22px",}} alt=''/>
-                </Button>
+            <div className='navbar-container' style={{marginLeft:'5%', marginRight:'5%', display:'flex', flexDirection:'column'}}>
+                <a href='/' rel='noopener'>
+                    <Button style={{ width:"100%",justifyContent:"flex-start", borderRadius:"16px",  maxWidth:"376px",marginTop:'5%', maxHeight:"83.22px",}}>
+                        <img src={LogoSvg} style={{minHeight:"83.22px",}} alt=''/>
+                        
+                    </Button>
                 </a>
-            <div className='text-container' style={{display:'flex', flexDirection:'column', justifyContent:'flex-start', marginTop:'120px'}}>
-                <div className='splash-header' style={{...titleTextstyle, maxWidth:"50%"}}>
-                    Tokenized real-world ecological assets
+            </div>
+            <div className='hero-body' style={{display:'flex', marginLeft:'5%', marginRight:'5%', marginTop:'120px', flexDirection:window.innerWidth < 768 ?'column':'row'}}>
+                <div className='text-container' style={{display: 'flex', flexDirection:'column', justifyContent:'flex-start', maxWidth:window.innerWidth < 768 ?'100%':'50%'}}>
+                    <div className='splash-header' style={{...titleTextstyle}}>
+                        Tokenized real-world ecological assets
+                    </div>
+                    <div className='splash-content' style={{...contentTextStyle, marginTop:"100px"}}>
+                        The platform to incentivise the collection/uploading of biodiversity genomic data, and labelling/annotation of it which will allow productive processing of data to be done. The platform aims to incentivise the conservation of biodiversity with the creation of revenue streams from the monetisation of this biogenome IP and value.
+                    </div>
                 </div>
-                <div className='splash-content' style={{...contentTextStyle, maxWidth:"50%", marginTop:"100px"}}>
-                    The platform to incentivise the collection/uploading of biodiversity genomic data, and labelling/annotation of it which will allow productive processing of data to be done. The platform aims to incentivise the conservation of biodiversity with the creation of revenue streams from the monetisation of this biogenome IP and value.
-                </div>
-                </div>
+            <img src={TreeSvg} style={{justifyContent:'flex-end', maxWidth:window.innerWidth < 768 ?'100%':'50%'}} alt=''/>
             </div>
             </div>           
             
