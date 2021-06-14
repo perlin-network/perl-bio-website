bd0ac0e12e4274dafb3d0e089e749b68b90f0afa
silvercondor <biocrypto@protonmail.com>
2021-06-14 15:58:21 +0800

    Add analytics

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -11,23 +11,21 @@ function Splash() {
             
             <div
             style={{
-                backgroundColor:'#333336',mixBlendMode:'pass-through',  backgroundImage: `url("data:image/svg+xml,${svgString}")`, backgroundSize: "cover", minHeight:'100%', paddingBottom:'200px'
+                backgroundColor:'#333336',mixBlendMode:'pass-through',  backgroundImage: `url("data:image/svg+xml,${svgString}")`, backgroundSize: "cover", minHeight:'100vh'
             }}
             > 
             <div className='content-container' style={{marginLeft:'5%', marginRight:'5%', display:'flex', flexDirection:'column'}}>
             <a href='/' rel='noopener'>
-            <div className='logo' style={{display:'flex', justifyContent:'flex-start', marginTop:'5%', maxHeight:"83.22px", maxWidth:"376px"}}>
-                <Button style={{width:'100%', justifyContent:"flex-start", borderRadius:"16px"}}>
-                <img src={LogoSvg} style={{minHeight:"83.22px",}} alt=''/>               
+                <Button style={{ width:"100%",justifyContent:"flex-start", borderRadius:"16px",  maxWidth:"376px",marginTop:'5%', maxHeight:"83.22px",}}>
+                    <img src={LogoSvg} style={{minHeight:"83.22px",}} alt=''/>
                 </Button>
-            </div>
                 </a>
             <div className='text-container' style={{display:'flex', flexDirection:'column', justifyContent:'flex-start', marginTop:'120px'}}>
                 <div className='splash-header' style={{...titleTextstyle, maxWidth:"50%"}}>
                     Tokenized real-world ecological assets
                 </div>
                 <div className='splash-content' style={{...contentTextStyle, maxWidth:"50%", marginTop:"100px"}}>
-                The platform to incentivise the collection/uploading of biodiversity genomic data, and labelling/annotation of it which will allow productive processing of data to be done. The platform aims to incentivise the conservation of biodiversity with the creation of revenue streams from the monetisation of this biogenome IP and value.
+                    The platform to incentivise the collection/uploading of biodiversity genomic data, and labelling/annotation of it which will allow productive processing of data to be done. The platform aims to incentivise the conservation of biodiversity with the creation of revenue streams from the monetisation of this biogenome IP and value.
                 </div>
                 </div>
             </div>
