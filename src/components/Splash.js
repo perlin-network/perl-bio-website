ecbc3cfa4129cbc1430c4cb769a87f1a84a28f72
silvercondor <biocrypto@protonmail.com>
2021-06-15 16:01:51 +0800

    Update text

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -23,10 +23,10 @@ function Splash() {
             <div className='hero-body' style={{display:'flex', marginLeft:'5%', marginRight:'5%', marginTop:'120px', flexDirection:window.innerWidth < 768 ?'column':'row'}}>
                 <div className='text-container' style={{display: 'flex', flexDirection:'column', justifyContent:'flex-start', maxWidth:window.innerWidth < 768 ?'100%':'50%'}}>
                     <div className='splash-header' style={{...titleTextstyle}}>
-                        Tokenized real-world ecological assets
+                        Build a sustainable future 
                     </div>
                     <div className='splash-content' style={{...contentTextStyle, marginTop:"100px"}}>
-                        The platform to incentivise the collection/uploading of biodiversity genomic data, and labelling/annotation of it which will allow productive processing of data to be done. The platform aims to incentivise the conservation of biodiversity with the creation of revenue streams from the monetisation of this biogenome IP and value.
+                    Bioeconomy is a leader in creating climate change solutions and developing strategies for ecological projects, including around carbon and biodiversity. This includes empowering organizations to realise opportunities with blockchain technologies.
                     </div>
                 </div>
             <img src={TreeSvg} style={{justifyContent:'flex-end', maxWidth:window.innerWidth < 768 ?'100%':'50%'}} alt=''/>
