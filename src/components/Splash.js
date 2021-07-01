d680ac2f54eb778987c655601d49fd667053fd80
silvercondor <biocrypto@protonmail.com>
2021-07-01 11:52:13 +0800

    Minor text change

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -22,7 +22,7 @@ function Splash() {
                         </Grid>
                         <Grid item>
                             <div className='splash-content' style={{...contentTextStyle, fontSize:'24px', marginTop:"100px", maxWidth:'550px'}}>
-                                Bioeconomy is a leader in creating climate change solutions and developing strategies for ecological projects, including around carbon and biodiversity. This includes empowering organizations to realise opportunities with blockchain technologies.
+                                Bioeconomy is a leader in creating climate change solutions and developing strategies for ecological projects, including around carbon and biodiversity. This includes empowering organisations to realise opportunities with blockchain technologies.
                             </div>
                         </Grid>
                         <Grid item style={{marginTop:'50px'}}>
@@ -35,22 +35,10 @@ function Splash() {
                     </Grid>                    
                 </Grid>
                 <Grid item justify='flex-end' >
-                    {/* <div style={{backgroundImage:`url(${TreeSvg})`,  backgroundSize:'cover'}}/> */}
                     <img src={TreeSvg} alt='' style={{maxWidth:'100%', minWidth:'100%', maxHeight:"720px", marginTop:window.innerWidth < 768 ?'0px':'-125px' }}/>
                 </Grid> 
             </Grid>
             </div>
-            {/* <div className='hero-body' style={{display:'flex', marginLeft:'5%', marginRight:'5%', flexDirection:window.innerWidth < 768 ?'column':'row'}}>
-                <div className='text-container' style={{display: 'flex', flexDirection:'column', justifyContent:'flex-start', maxWidth:window.innerWidth < 768 ?'100%':'50%'}}>
-                    <div className='splash-header' style={{...titleTextstyle}}>
-                        Build a sustainable future 
-                    </div>
-                    <div className='splash-content' style={{...contentTextStyle, marginTop:"100px"}}>
-                    Bioeconomy is a leader in creating climate change solutions and developing strategies for ecological projects, including around carbon and biodiversity. This includes empowering organizations to realise opportunities with blockchain technologies.
-                    </div>
-                </div>
-            <img src={TreeSvg} style={{justifyContent:'flex-end', maxWidth:window.innerWidth < 768 ?'100%':'50%'}} alt=''/>
-            </div> */}
             <div className='partner-container' style={{marginTop:'75px', maxWidth:'100%',margin:'0% 5%'}}>
                 <p style={{...contentTextStyle, color:"#A5AFA8", }}>Partners</p>
                 <Grid container direction='column'>
