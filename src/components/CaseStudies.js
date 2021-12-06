2ecb71fdfb5af3d01aad8d88cbf9872667fade27
silvercondor <biocrypto@protonmail.com>
2021-12-06 20:16:16 +0800

    refactor: change project titles

--- src/components/CaseStudies.js
+++ src/components/CaseStudies.js
@@ -8,18 +8,18 @@ function CaseStudies() {
     let gridItems=[
         {
             icon:PictureLeft,
-            title:`North Pikounda REDD+ Project`,
+            title:`Gorilla Project`,
             content:`
-            The North Pikounda REDD+ Project (NPR+) is the first voluntary REDD+ project in the Republic of Congo (RoC) that will reduce GHG emissions by avoiding deforestation and degradation under the VCS Standard. The NPR+ Project began as an idea in 2010, and formally began its crediting period in 2012; it is the initial REDD+ Pilot project of the Republic of Congo. The Project protects 92,530 hectares of pristine unlogged rainforest. 
+            The Gorilla Project is the first voluntary REDD+ project in the Republic of Congo (RoC) that will reduce GHG emissions by avoiding deforestation and degradation under the VCS Standard. The NPR+ Project began as an idea in 2010, and formally began its crediting period in 2012; it is the initial REDD+ Pilot project of the Republic of Congo. The Project protects 92,530 hectares of pristine unlogged rainforest. 
 
 The NPR+ project area contains a total of 22 protected species considered by the IUCN & CITES. Such as the Lowland Gorilla (Gorilla gorilla gorilla), African Forest Elephant (Loxodonta cyclotis), Hippopotamus (Hippopotamus amphibius) & Leopard (Panthera pardus)
             `
         },
         {
             icon:PictureRight,
-            title:`Riau Ecosystem Restoration (RER) Carbon Project`,
+            title:`Tiger Project`,
             content:`
-            The Riau Ecosystem Restoration Carbon Project seeks to protect and restore 130,090 ha of peatland ecosystem located in the Republic of Indonesia while at the same time preserving biodiversity and enhancing local livelihoods. The Project will avoid 78,425 ha of forest clearing and conversion to an industrial forestry plantation. The Project is on the island of Sumatra and in the Province of Riau. Specifically, it is located on the Kampar Peninsula, which is a rain-fed peatland forest made up of four former logging concessions. 
+            The Tiger Project seeks to protect and restore 130,090 ha of peatland ecosystem located in the Republic of Indonesia while at the same time preserving biodiversity and enhancing local livelihoods. The Project will avoid 78,425 ha of forest clearing and conversion to an industrial forestry plantation. The Project is on the island of Sumatra and in the Province of Riau. Specifically, it is located on the Kampar Peninsula, which is a rain-fed peatland forest made up of four former logging concessions. 
 
             Through the REDD+ process of avoiding planned deforestation and wetlands conservation and restoration, the Project will be preserving the remaining peat swamp forests by preventing their conversion to forestry plantations, containing a total of 797 species of animals and plants. These species include the Sumatran Tiger (Panthera tigris sumatrae) & Storm's Stork (Ciconia stormi).
             
