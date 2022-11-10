66de724c017fc940f415f67235e0e328f3bcbad3
Michael Blishch <mike@bioeconomy.co>
2022-11-10 12:55:15 +0200

    updated imports

--- src/pages/Work/index.js
+++ src/pages/Work/index.js
@@ -6,7 +6,7 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
-import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
+// import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
 import ReactPlayer from 'react-player/lazy';
 
 import { ReactComponent as LogoSvg } from '../../assets/logo.svg';
