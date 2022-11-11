9cdbe372349b61e383104be98eed63661a42db69
Michael Blishch <mike@bioeconomy.co>
2022-11-11 20:52:50 +0200

    updated contact cards

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,13 +1,21 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 import { useParams } from 'react-router-dom';
-import { Typography, Box, Link } from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
 
-import LogoSvg from '../assets/logo.svg';
-import DorjeePng from '../assets/team/dorjee-sun-card.png';
+import { ReactComponent as LogoSvg } from '../assets/logo.svg';
+import BackgroundImage from '../assets/card-background.png';
+import Default from '../assets/team/default.png';
+import DorjeePng from '../assets/team/dorjee-sun.png';
 import DorjeeQr from '../assets/team/dorjee-sun-qr.png';
 import ByronPng from '../assets/team/byron-grigoratos.png';
 import ByronQr from '../assets/team/byron-grigoratos-qr.png';
 import AmandaQr from '../assets/team/amanda-qr.png';
+import DarrenPng from '../assets/team/darren-toh.png';
 import DarrenQr from '../assets/team/darren-qr.png';
 import MarkPng from '../assets/team/mark-harding.png';
 import MarkQr from '../assets/team/mark-harding-qr.png';
@@ -17,6 +25,10 @@ import JoPng from '../assets/team/jo.png';
 import JoQr from '../assets/team/jo-qr.png';
 //import LynnPng from '../assets/team/lynn.png';
 import LynnQr from '../assets/team/lynn-qr.png';
+import GarryPng from '../assets/team/garry-walsh.png';
+import GarryQr from '../assets/team/garry-walsh-qr.png';
+import PaulPng from '../assets/team/dr-paul-elkan.png';
+import PaulQr from '../assets/team/dr-paul-elkan-qr.png';
 
 const companyData = {
   company: 'Bioeconomy Pte. Ltd.',
@@ -30,10 +42,10 @@ const companyData = {
 const cards = {
   dorjee: {
     id: 'dorjee',
-    name: 'Dorjee Sun',
-    title: 'Chief Executive Officer',
+    name: 'Dorjee\nSun',
+    title: 'Chief Executive\nOfficer',
     email: 'dorjee@bioeconomy.co',
-    phone: '+6597770416',
+    phone: '+65 9777 0416',
     image: DorjeePng,
     qrCode: DorjeeQr,
     ...companyData,
@@ -41,176 +53,226 @@ const cards = {
   byron: {
     id: 'byron',
     name: 'Byron Grigoratos',
-    title: 'Chief Commercial Officer',
+    title: 'Chief Commercial\nOfficer',
     email: 'byron@bioeconomy.co',
-    phone: '+6584976218',
+    phone: '+65 8497 6218',
     image: ByronPng,
     qrCode: ByronQr,
     ...companyData,
   },
   amanda: {
     id: 'amanda',
-    name: 'Amanda Huynh',
-    title: 'Project Manager',
+    name: 'Amanda\nHuynh',
+    title: 'Project\nManager',
     email: 'amanda@bioeconomy.co',
-    phone: '+6588948041',
+    phone: '+65 8894 8041',
     image: null,
     qrCode: AmandaQr,
     ...companyData,
   },
   darren: {
     id: 'darren',
-    name: 'Darren Toh',
-    title: 'Project Director',
+    name: 'Darren\nToh',
+    title: 'Project\nDirector',
     email: 'darren@bioeconomy.co',
-    phone: '+6594289181',
-    image: null,
+    phone: '+65 9428 9181',
+    image: DarrenPng,
     qrCode: DarrenQr,
     ...companyData,
   },
   mark: {
     id: 'mark',
-    name: 'Mark Harding',
-    title: 'Carbon Legal Officer',
+    name: 'Mark\nHarding',
+    title: 'Head of\nProjects',
     email: 'mark@bioeconomy.co',
-    phone: '+886912316708',
+    phone: '+886 912 316 708',
     image: MarkPng,
     qrCode: MarkQr,
     ...companyData,
   },
   ralph: {
     id: 'ralph',
-    name: 'Ralph Strebel',
-    title: 'Head of Carbon & Legal Counsel',
+    name: 'Ralph\nStrebel',
+    title: 'Head of Carbon &\nLegal Counsel',
     email: 'ralph@bioeconomy.co',
-    phone: '+13034763618',
+    phone: '+1 303 476 36 18',
     image: RalphPng,
     qrCode: RalphQr,
     ...companyData,
   },
   jo: {
     id: 'jo',
-    name: 'Jo Huynh',
-    title: 'Corporate Affairs Director',
+    name: 'Jo\nHuynh',
+    title: 'Corporate Affairs\nDirector',
     email: 'jo@bioeconomy.co',
-    phone: '+6594888156',
+    phone: '+65 9488 8156',
     image: JoPng,
     qrCode: JoQr,
     ...companyData,
   },
   lynn: {
     id: 'lynn',
-    name: 'Lynn Lieu',
-    title: 'Finance and Admin',
-    email: 'admin@bioeconomy.co',
-    phone: '+6596743689',
+    name: 'Lynn\nLieu',
+    title: 'Finance and\nAdmin',
+    email: 'lynn@bioeconomy.co',
+    phone: '+65 9674 3689',
     qrCode: LynnQr,
+    ...companyData,
+  },
+  garry: {
+    id: 'garry',
+    name: 'Garry\nWalsh',
+    title: 'Chief Communications\nOfficer',
+    email: 'garry@bioeconomy.co',
+    phone: '+65 9379 4275',
+    image: GarryPng,
+    qrCode: GarryQr,
+    ...companyData,
+  },
+  paul: {
+    id: 'paul',
+    name: 'Paul\nElkan',
+    title: 'Chief Conservation\nOfficer',
+    email: 'paul@bioeconomy.co',
+    phone: '+1 646 785 7379',
+    image: PaulPng,
+    qrCode: PaulQr,
     ...companyData,
   },
 };
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: '360px',
-    margin: '0 auto',
-    padding: '30px',
-    '& .MuiLink-root': {
-      color: theme.palette.primary.light,
-    },
-  },
-  logo: {
-    display: 'block',
-    padding: '0 0 30px',
-    '& > img': {
-      display: 'block',
-      margin: '0 auto',
-    },
-  },
-  box: {
-    position: 'relative',
-    margin: '60px 0',
-    padding: '0 30px 30px',
-    color: '#fff',
-    // backgroundColor: 'hsl(240deg 3% 25%)',
-    backgroundColor: theme.palette.background.accent,
-    borderRadius: 10,
-    boxShadow: '0 0 20px rgba(0, 0, 0, 0.15)',
-  },
-  image: {
-    marginTop: -60,
-    width: 120,
-    borderRadius: 16,
-  },
-  name: {
-    margin: '16px 0 0',
-    color: theme.palette.primary.light,
-    fontSize: '2rem',
-    fontWeight: 700,
-  },
-  title: {
-    margin: '0 0 16px',
-    fontSize: '1.25rem',
-    fontWeight: 700,
-  },
-  company: { marginTop: 16, fontWeight: 700 },
-  qrCode: {
-    marginTop: 30,
-    maxWidth: '100%',
-    borderRadius: 10,
-  },
-}));
-
 export default function ContactCard() {
-  const classes = useStyles();
   const { cardId } = useParams();
   const data = cards[cardId];
   return (
-    <div className={classes.root}>
-      <Link href="/" className={classes.logo}>
-        <img src={LogoSvg} style={{ height: 54 }} alt="Bioeconomy" />
-      </Link>
-      <Box className={classes.box}>
-        {data.image && (
-          <img className={classes.image} src={data.image} alt={data.name} />
-        )}
-        <Typography
-          className={classes.name}
-          variant="h1"
-          style={{ paddingTop: data.image ? 0 : 30 }}
-        >
-          {data.name}
-        </Typography>
-        <Typography className={classes.title} variant="h2">
-          {data.title}
-        </Typography>
-        <Typography>
-          <Link className={classes.email} href={`mailto:${data.email}`}>
-            {data.email}
-          </Link>
-        </Typography>
-        <Typography>
-          <Link className={classes.phone} href={`tel:${data.phone}`}>
-            {data.phone}
-          </Link>
-        </Typography>
-        <Typography className={classes.company}>{data.company}</Typography>
-        {data.address.split('\n').map((addressLine, key) => (
-          <Typography key={key} paragraph style={{ margin: 0 }}>
-            {addressLine}
-          </Typography>
-        ))}
-        <Typography>
+    <Root>
+      <Container maxWidth={false} disableGutters sx={{ maxWidth: 330 }}>
+        <Card>
           <Link
-            className={classes.website}
-            href={`https://${data.website}`}
-            rel="noopener noreferrer"
-            target="_blank"
+            href="/"
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              height: 36,
+              my: 3,
+            }}
           >
-            {data.website}
+            <LogoSvg />
           </Link>
-        </Typography>
-        <img className={classes.qrCode} src={data.qrCode} alt="QR code" />
-      </Box>
-    </div>
+          <Box pt={3} pb={5}>
+            <ProfilePicture src={data.image || Default} alt={data.name} />
+            <Name>{data.name}</Name>
+            <Title>{data.title}</Title>
+            <Box my={2}>
+              <ContactLink
+                href={`mailto:${data.email}`}
+                sx={{ textDecorationColor: '#FFF' }}
+              >
+                {data.email}
+              </ContactLink>
+              <ContactLink
+                href={`tel:${data.phone.split(' ').join('')}`}
+                underline="none"
+              >
+                {data.phone}
+              </ContactLink>
+            </Box>
+            <Paragraph>{data.company}</Paragraph>
+            {data.address.split('\n').map((addressLine, key) => (
+              <Paragraph key={key}>{addressLine}</Paragraph>
+            ))}
+            <Paragraph>
+              <Link
+                href={`https://${data.website}`}
+                rel="noopener noreferrer"
+                target="_blank"
+                sx={{ textDecorationColor: '#FFF' }}
+              >
+                {data.website}
+              </Link>
+            </Paragraph>
+          </Box>
+          <Box display="flex">
+            <QRCode src={data.qrCode} />
+          </Box>
+        </Card>
+      </Container>
+    </Root>
   );
 }
+
+const Root = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(3),
+  background: '#FFF',
+}));
+
+const Card = styled(Paper)(({ theme }) => ({
+  position: 'relative',
+  padding: theme.spacing(2.5),
+  color: '#fff',
+  background: `#111112 url(${BackgroundImage}) no-repeat`,
+  backgroundSize: 'cover',
+  boxShadow: '0px 0px 10px rgba(18, 56, 102, 0.3)',
+  borderRadius: 16,
+}));
+
+const ProfilePicture = styled((props) => (
+  <Box {...props}>
+    <img src={props.src} alt={props.alt} />
+  </Box>
+))(({ theme }) => ({
+  marginBottom: theme.spacing(2),
+  width: 164,
+  height: 164,
+  img: {
+    maxWidth: '100%',
+    height: '100%',
+    borderRadius: 16,
+  },
+}));
+
+const Name = styled((props) => (
+  <Typography
+    variant="h1"
+    fontSize={40}
+    fontWeight={600}
+    whiteSpace="pre-line"
+    {...props}
+  />
+))();
+
+const Title = styled((props) => (
+  <Typography
+    variant="h2"
+    pt={2}
+    pb={3}
+    fontSize={24}
+    fontWeight={600}
+    whiteSpace="pre-line"
+    {...props}
+  />
+))({
+  position: 'relative',
+  '&::after': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    bottom: 0,
+    height: 4,
+    width: 124,
+    background: '#E9E9ED',
+  },
+});
+
+const ContactLink = styled((props) => (
+  <Link display="block" fontSize={20} fontWeight={600} {...props} />
+))();
+
+const Paragraph = (props) => (
+  <Typography fontSize={20} fontWeight={600} {...props} />
+);
+
+const QRCode = styled('img')(({ theme }) => ({
+  maxWidth: '100%',
+  borderRadius: 16,
+}));
